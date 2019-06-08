e515398ad57fe6b74e97b9867f4a93cf76562828
Giorgi Darchiashvili <gdar91@live.com>
2019-06-08 13:12:37 +0400

    added the solution.

---
+++ CopyAndUpdate/Extensions.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace CopyAndUpdate
+{
+    public static class Extensions
+    {
+        public static TObject With<TObject, TValue>(
+            this TObject target,
+            Expression<Func<TObject, TValue>> memberSelector,
+            TValue newValue
+        )
+            => WithModule.Template(target, (memberSelector, newValue));
+
+        public static TObject With<TObject, TValue1, TValue2>(
+            this TObject target,
+            Expression<Func<TObject, TValue1>> memberSelector1,
+            TValue1 newValue1,
+            Expression<Func<TObject, TValue2>> memberSelector2,
+            TValue2 newValue2
+        )
+            => WithModule.Template(
+                target,
+                (memberSelector1, newValue1),
+                (memberSelector2, newValue2)
+            );
+        
+        public static TObject With<TObject, TValue1, TValue2, TValue3>(
+            this TObject target,
+            Expression<Func<TObject, TValue1>> memberSelector1,
+            TValue1 newValue1,
+            Expression<Func<TObject, TValue2>> memberSelector2,
+            TValue2 newValue2,
+            Expression<Func<TObject, TValue3>> memberSelector3,
+            TValue3 newValue3
+        )
+            => WithModule.Template(
+                target,
+                (memberSelector1, newValue1),
+                (memberSelector2, newValue2),
+                (memberSelector3, newValue3)
+            );
+
+        public static TObject With<TObject, TValue1, TValue2, TValue3, TValue4>(
+            this TObject target,
+            Expression<Func<TObject, TValue1>> memberSelector1,
+            TValue1 newValue1,
+            Expression<Func<TObject, TValue2>> memberSelector2,
+            TValue2 newValue2,
+            Expression<Func<TObject, TValue3>> memberSelector3,
+            TValue3 newValue3,
+            Expression<Func<TObject, TValue4>> memberSelector4,
+            TValue4 newValue4
+        )
+            => WithModule.Template(
+                target,
+                (memberSelector1, newValue1),
+                (memberSelector2, newValue2),
+                (memberSelector3, newValue3),
+                (memberSelector4, newValue4)
+            );
+        
+        public static TObject With<TObject, TValue1, TValue2, TValue3, TValue4, TValue5>(
+            this TObject target,
+            Expression<Func<TObject, TValue1>> memberSelector1,
+            TValue1 newValue1,
+            Expression<Func<TObject, TValue2>> memberSelector2,
+            TValue2 newValue2,
+            Expression<Func<TObject, TValue3>> memberSelector3,
+            TValue3 newValue3,
+            Expression<Func<TObject, TValue4>> memberSelector4,
+            TValue4 newValue4,
+            Expression<Func<TObject, TValue5>> memberSelector5,
+            TValue5 newValue5
+        )
+            => WithModule.Template(
+                target,
+                (memberSelector1, newValue1),
+                (memberSelector2, newValue2),
+                (memberSelector3, newValue3),
+                (memberSelector4, newValue4),
+                (memberSelector5, newValue5)
+            );
+
+        public static TObject With<TObject, TValue1, TValue2, TValue3, TValue4, TValue5, TValue6>(
+            this TObject target,
+            Expression<Func<TObject, TValue1>> memberSelector1,
+            TValue1 newValue1,
+            Expression<Func<TObject, TValue2>> memberSelector2,
+            TValue2 newValue2,
+            Expression<Func<TObject, TValue3>> memberSelector3,
+            TValue3 newValue3,
+            Expression<Func<TObject, TValue4>> memberSelector4,
+            TValue4 newValue4,
+            Expression<Func<TObject, TValue5>> memberSelector5,
+            TValue5 newValue5,
+            Expression<Func<TObject, TValue6>> memberSelector6,
+            TValue6 newValue6
+        )
+            => WithModule.Template(
+                target,
+                (memberSelector1, newValue1),
+                (memberSelector2, newValue2),
+                (memberSelector3, newValue3),
+                (memberSelector4, newValue4),
+                (memberSelector5, newValue5),
+                (memberSelector6, newValue6)
+            );
+
+        public static TObject With<TObject, TValue1, TValue2, TValue3, TValue4, TValue5, TValue6, TValue7>(
+            this TObject target,
+            Expression<Func<TObject, TValue1>> memberSelector1,
+            TValue1 newValue1,
+            Expression<Func<TObject, TValue2>> memberSelector2,
+            TValue2 newValue2,
+            Expression<Func<TObject, TValue3>> memberSelector3,
+            TValue3 newValue3,
+            Expression<Func<TObject, TValue4>> memberSelector4,
+            TValue4 newValue4,
+            Expression<Func<TObject, TValue5>> memberSelector5,
+            TValue5 newValue5,
+            Expression<Func<TObject, TValue6>> memberSelector6,
+            TValue6 newValue6,
+            Expression<Func<TObject, TValue7>> memberSelector7,
+            TValue7 newValue7
+        )
+            => WithModule.Template(
+                target,
+                (memberSelector1, newValue1),
+                (memberSelector2, newValue2),
+                (memberSelector3, newValue3),
+                (memberSelector4, newValue4),
+                (memberSelector5, newValue5),
+                (memberSelector6, newValue6),
+                (memberSelector7, newValue7)
+            );
+    }
+}
---
+++ CopyAndUpdate/WithModule.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace CopyAndUpdate
+{
+    public static class WithModule
+    {
+        internal static TObject Template<TObject>(
+            TObject target,
+            params (Expression memberSelector, object newValue)[] changes
+        )
+        {
+            var actions = new List<Action<TObject>>();
+
+            foreach (var (memberSelector, newValue) in changes)
+            {
+                if (!(
+                    memberSelector.NodeType == ExpressionType.Lambda &&
+                    memberSelector is LambdaExpression lambdaExpression &&
+                    lambdaExpression.Body is MemberExpression body &&
+                    body.NodeType == ExpressionType.MemberAccess &&
+                    body.Expression.NodeType == ExpressionType.Parameter
+                ))
+                {
+                    throw new Exception("The expression should be a lambda, " +
+                        "accessing specific property or field of the target.");
+                }
+
+                switch (body.Member)
+                {
+                    case PropertyInfo propertyInfo:
+                        HandlePropertyInfo(newValue, propertyInfo);
+                        break;
+
+                    case FieldInfo fieldInfo:
+                        HandleFieldInfo(newValue, fieldInfo);
+                        break;
+
+                    default:
+                        throw new Exception("Something has changed in the reflection library.");
+                }
+            }
+
+            if (!actions.Any())
+            {
+                return target;
+            }
+
+            var targetClone = Clone(target);
+
+            actions.ForEach(action => action(targetClone));
+
+            return targetClone;
+
+
+            void HandlePropertyInfo(object newValue, PropertyInfo propertyInfo)
+            {
+                if (propertyInfo.CanRead)
+                {
+                    var value = propertyInfo.GetValue(target);
+
+                    if (object.Equals(newValue, value))
+                    {
+                        return;
+                    }
+
+                    if (!propertyInfo.CanWrite)
+                    {
+                        if (TryFindPropertyBackingField(target, propertyInfo, newValue,
+                            out var fieldInfo))
+                        {
+                            actions.Add(clone => fieldInfo.SetValue(clone, newValue));
+                            
+                            return;
+                        }
+                        else
+                        {
+                            throw new Exception("Can not write to a property " +
+                                "with no setter and no backing field.");
+                        }
+                    }
+                }
+
+                actions.Add(clone => propertyInfo.SetValue(clone, newValue));
+            }
+
+            void HandleFieldInfo(object newValue, FieldInfo fieldInfo)
+            {
+                if (!object.Equals(newValue, fieldInfo.GetValue(target)))
+                {
+                    actions.Add(clone => fieldInfo.SetValue(clone, newValue));
+                }
+            }
+        }
+
+        private static TObject Clone<TObject>(TObject target)
+        {
+            var bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+            var methodInfo = typeof(object).GetMethod(nameof(MemberwiseClone), bindingFlags);
+            var targetClone = (TObject) methodInfo.Invoke(target, default);
+
+            return targetClone;
+        }
+
+        private static IEnumerable<FieldInfo> AllFields<TObject>()
+        {
+            var bindingFlags =
+                BindingFlags.Instance |
+                BindingFlags.DeclaredOnly |
+                BindingFlags.Public |
+                BindingFlags.NonPublic;
+
+            var type = typeof(TObject);
+
+            while (type != null)
+            {
+                foreach (var fieldInfo in type.GetFields(bindingFlags))
+                {
+                    yield return fieldInfo;
+                }
+
+                type = type.BaseType;
+            }
+        }
+
+
+        private static Dictionary<PropertyInfo, FieldInfo> _propertyBackingFields
+            = new Dictionary<PropertyInfo, FieldInfo>();
+
+        private static bool TryFindPropertyBackingField<TObject>(
+            TObject target,
+            PropertyInfo propertyInfo,
+            object newValue,
+            out FieldInfo resultFieldInfo
+        )
+        {
+            var foundFieldInfos = _propertyBackingFields.ContainsKey(propertyInfo)
+                ? new[] { _propertyBackingFields[propertyInfo] }
+                : new FieldInfo[0];
+
+            var propertyType = propertyInfo.PropertyType;
+
+            resultFieldInfo = foundFieldInfos.Concat(AllFields<TObject>())
+                .FirstOrDefault(fieldInfo =>
+                {
+                    if (!fieldInfo.FieldType.IsAssignableFrom(propertyType))
+                    {
+                        return false;
+                    }
+
+                    var targetClone = Clone(target);
+
+                    fieldInfo.SetValue(targetClone, newValue);
+
+                    var testValue = propertyInfo.GetValue(targetClone);
+
+                    return object.Equals(newValue, testValue);
+                });
+            
+            _propertyBackingFields[propertyInfo] = resultFieldInfo;
+            
+            return resultFieldInfo != default;
+        }
+    }
+}
